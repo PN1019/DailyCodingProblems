43c128e1d55ec8123d103d0b7448522c5d9ddf7f
Purva <nahar.purva@outlook.com>
2021-09-23 19:01:18 +0530

    Update linkedIn_find_nearest_k_points_from_central_point.py

--- linkedIn_find_nearest_k_points_from_central_point.py
+++ linkedIn_find_nearest_k_points_from_central_point.py
@@ -14,8 +14,11 @@ sol=Solution()
 points = [[3, 3], [5, -1], [-2, 4]]
 #points=[[0, 0], [5, 4], [3, 1]]
 K = 2
- 
-print(sol.kClosest(points, K))        
+print(f"The nearest {K} neighbors {points} from the central point are{sol.kClosest(points, K)}")  
+#result1=[[-2, 4], [3, 3]]
+#result2=[(0, 0), (3, 1)]
+    
+       
 #Time complexity: O(NlogN)
 #Sort the list according to the distance to origin. 
 #Apparently, we did more than the question asked. We sorted all the distance, the question only ask for top k.
@@ -39,10 +42,10 @@ sol=Solution()
 #points2 = [[3, 3], [5, -1], [-2, 4]]
 points1=[[0, 0], [5, 4], [3, 1]]
 K = 2
- 
-print(sol.kClosest(points1, K))        
-#result1=[(0, 0), (3, 1)]
-#result2=[[-2, 4], [3, 3]]    
+print(f"The nearest {K} neighbors {points} from the central point are{sol.kClosest(points, K)}")  
+#result1=[[-2, 4], [3, 3]]
+#result2=[(0, 0), (3, 1)]
+       
     
 # Time compiexity: O(NlogK)
 # heapq is a binary heap, with O(log n) push and O(log n) pop. n is the size of the minimum heap. In this case, n = K.
@@ -76,8 +79,38 @@ sol=Solution()
 points = [[3, 3], [5, -1], [-2, 4]]
 #points=[[0, 0], [5, 4], [3, 1]]
 K = 2
- 
-print(sol.kClosest(points, K))
+print(f"The nearest {K} neighbors {points} from the central point are{sol.kClosest(points, K)}")  
+#result1=[[-2, 4], [3, 3]]
+#result2=[(0, 0), (3, 1)]
 #Time Complexity: Quick Sort - O(N) on average
+     
+#Solution by Kd-tree Approach for large no. of queries:
+# What if I have 10 million points now and I have to perform the search 10000 times? How would you optimize it?
+from scipy import spatial
+from typing import List
+
+class Solution:
+    def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
+        tree = spatial.KDTree(points)
+		# x is the origin, k is the number of closest neighbors, p=2 refers to choosing l2 norm (euclidean distance)
+        distance, idx = tree.query(x=[0,0], k=K, p=2) 
+        return [points[i] for i in idx] if K > 1 else [points[idx]]
+sol=Solution()
+#points = [[3, 3], [5, -1], [-2, 4]]
+points=[[1, 3], [5, 4], [3, 1]]
+K = 2
+ 
+print(f"The nearest {K} neighbors {points} from the central point are{sol.kClosest(points, K)}")        
+#result1=[[-2, 4], [3, 3]]
+#result2=[(1, 3), (3, 1)]
+
+
+# kd-tree has the following comeplexity:
+
+# Build the tree - O(NlogN), building the tree requires presorting the points and find the medians (but we only need to do this once).
+# Search, Insert, Delete - O(logN), similar to how a normal binary tree works (with a tree balancing mechanism)
+# Now, as we can see, it greatly reduces the time complexity for each nearest neighbor query to O(logN), and if we need to find the K closest points, the total complexity will be O(KlogN).
+# This is great if we have a lot of points and we are only interested in a few neighbors.
+
 
 
